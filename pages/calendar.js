438d542586d0d84bc619f2e0d80d5e047247169e
victor Okinyi <jets.starplus@gmail.com>
2021-10-19 17:00:13 +0300

    calendar page integration

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,25 +1,35 @@
-import React from 'react'
+import React from 'react';
 
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import CalendarPicker from '@mui/lab/CalendarPicker';
-import MonthPicker from '@mui/lab/MonthPicker';
 import Grid from '@mui/material/Grid';
-import {Typography} from '@mui/material'
+import {Typography, Box} from '@mui/material';
 
-import Head from 'next/head'
+import Head from 'next/head';
 import { isWeekend } from 'date-fns';
 
-import Layout from '../components/layout'
-import theme from '../src/theme'
+import { Calendar, Views, momentLocalizer } from 'react-big-calendar';
+import events from '../data/occurences';
+import moment from 'moment';
+
+
+import * as dates from '../utils/dates';
+
+import Layout from '../components/layout';
+import theme from '../src/theme';
+
+let allViews = Object.keys(Views).map(k => Views[k])
+
 
 const minDate = new Date('2020-01-01T00:00:00.000');
 const maxDate = new Date('2034-01-01T00:00:00.000');
 
+const months = [0, 1, 2, 3, 4 ,5 ,6, 7, 8, 9, 10, 11]
+const localizer = momentLocalizer(moment);
 
-
-const Calendar = () => {
-    const [date, setDate] = React.useState(new Date());
+const Calendar2 = () => {
+    
     return (
         <>
             <Head>
@@ -35,29 +45,31 @@ const Calendar = () => {
                     Calendar
             </Typography>
                 <LocalizationProvider dateAdapter={AdapterDateFns}>
-                    <Grid container spacing={3}>
-                    <Grid item xs={12} md={6}>
-                        <CalendarPicker date={date} shouldDisableDate={isWeekend} onChange={(newDate) => setDate(newDate)} />
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <MonthPicker
-                        date={date}
-                        minDate={minDate}
-                        maxDate={maxDate}
-                        onChange={(newDate) => setDate(newDate)}
-                        sx={{
-                            backgroundColor: theme.palette.primary.main, 
-                            p: .5,
-                            borderRadius: 1
-                        }}
-                        />
-                    </Grid>
+                    <Grid container>
+                    {months.map(month=>{
+                        return(
+                        <Grid item xs={12} md={4} sm={6}>
+                            {/* <CalendarPicker date={new Date().setMonth(month)} shouldDisableDate={isWeekend} onchange={null}/> */}
+                        </Grid>
+                        )})
+                    }  
                     
                     </Grid>
                 </LocalizationProvider>
+                <Box sx={{height: 600}}>
+                <Calendar
+                    events={events}
+                    views={allViews}
+                    step={60}
+                    showMultiDayTimes
+                    max={dates.add(dates.endOf(new Date(), 'day'), -1, 'hours')}
+                    defaultDate={new Date()}
+                    localizer={localizer}
+                    />
+                </Box>
         </Layout> 
         </>
     )
 }
 
-export default Calendar
+export default Calendar2
